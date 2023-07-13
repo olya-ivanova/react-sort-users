de4b6b4e48fd03c6de4c702f5f2e1a7a04322780
Volha Ivanova <Volha.Ivanova@itechart-group.com>
2023-07-13 23:33:49 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+const OPTIONS = [
+  { value: "name", label: "by name" },
+  { value: "username", label: "by username" },
+  { value: "email", label: "by email" },
+];
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [selected, setSelected] = useState("");
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.log(error.message));
+  }, []);
+
+  const handleSort = (sortValue) => {
+    setSelected(sortValue);
+    setUsers(
+      [...users].sort((a, b) => a[sortValue].localeCompare(b[sortValue]))
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <select
+        value={selected}
+        onChange={(event) => handleSort(event.target.value)}
+      >
+        <option value="" disabled>
+          Sort by
+        </option>
+        {OPTIONS.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+      <div>
+        {users &&
+          users.map((user) => (
+            <div
+              style={{
+                margin: "16px",
+                padding: "16px",
+                border: "2px solid #08F9A5",
+              }}
+              key={user.id}
+            >
+              <h2>{user.name}</h2>
+              <p>{user.username}</p>
+              <p>{user.email}</p>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
